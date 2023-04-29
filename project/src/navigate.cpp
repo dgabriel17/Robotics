fbd034c3040047521711ece03da474b2821506db
C-Gauth <114773300+C-Gauth@users.noreply.github.com>
2023-04-28 21:15:47 -0700

    added sub/loop

--- project/src/navigate.cpp
+++ project/src/navigate.cpp
@@ -1,12 +1,61 @@
 #include <rclcpp/rclcpp.hpp> 
 #include <navigation/navigation.hpp>
-#include <nav_msgs/msg/occupancy_grid.hpp>
 #include <iostream>
+
+rclcpp::Node::SharedPtr nodeh;
+bool first = true;
+nav_msgs::msg::OccupancyGrid::SharedPtr first_map;
+
+// Callback function 
+void callback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
+   //print received string to the screen
+   if (first){ //if first time get original map
+	   first_map = msg;
+	   first = false;
+   }
+
+  RCLCPP_INFO(nodeh->get_logger(),"Map Data: %s",msg->data);
+}
+
+// Callback function for map
+void mapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg) {
+  // loop through the map.......................
+  int x,y,diff, arrLoc;
+  for (int w = 0; w < msg->info.height; ++w){
+  		for ( int h=0; h < msg->info.height; h++){
+  			// find x/y for the big map
+  			x = (w* msg->info.resolution)+ (msg->info.resolution /2) - 10 ;
+  			y = (h* msg->info.resolution) + ( msg->info.resolution /2 ) - 10;
+			//calculate position in 1 by ~~~ array
+  			arrLoc = (y * 384) + x -10;
+  			//check for differences between current and first map
+  			diff = first_map->data[arrLoc] - msg->data[arrLoc];
+  			if (diff > 50){
+				std::cout << "somethings fishy here" << x <<", " << y << std::endl;
+			}  			
+  			
+  		}
+   }
+}
+
 
 int main(int argc,char **argv) {
  
   rclcpp::init(argc,argv); // initialize ROS 
   Navigator navigator(true,false); // create node with debug info but not verbose
+  
+  // Create instance of a node
+  nodeh = rclcpp::Node::make_shared("mapVals");
+  
+  // Subscribe to the laser data
+  rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr laser_sub;
+  	
+  // subscribe to topic "stringm" an register the callback function
+  laser_sub = nodeh->create_subscription<nav_msgs::msg::OccupancyGrid>
+                                        ("mapVals",10,&callback);
+                                        
+   //subscriber to MAP
+   auto sub = nodeh->create_subscription<nav_msgs::msg::OccupancyGrid>("global_costmap", 10, &mapCallback);
 
   // first: it is mandatory to initialize the pose of the robot
   geometry_msgs::msg::Pose::SharedPtr init = std::make_shared<geometry_msgs::msg::Pose>();
@@ -37,18 +86,19 @@ int main(int argc,char **argv) {
   	goal_pos->orientation.w = 1;
   	// move to new pose
   	navigator.GoToPose(goal_pos);
-  	while ( ! navigator.IsTaskComplete() ) 
-  	{
-  	
-  	}
+  	while ( ! navigator.IsTaskComplete() ) {}
   	
   	// Increment arr_index
   	arr_index++;
+  	
+    // wait for messages and process them                
+    rclcpp::spin(nodeh); 
   	
   	// backup of 0.15 m (deafult distance)
   	navigator.Backup();
   	while ( ! navigator.IsTaskComplete() ) {}
    }
+   
    
   float arr_loc_x_two[] = {-0.5, 0.5, 0.5, -0.5};
   float arr_loc_y_two[] = {0.5, 0.5, -0.5, -0.5};
@@ -74,7 +124,15 @@ int main(int argc,char **argv) {
   	navigator.Backup();
   	while ( ! navigator.IsTaskComplete() ) {}
    }
-  // complete here....
+   
+
+
+  
+  //MRTP CH7 has the laser scan info
+  // Use Global cost map, it will show abnormalities on the map
+  // Type is mav_msgs/msg/Ocupancy grid
+  // Check the array then check the next instance and look for differences, move toward the differences till we're at the pole
+  // Next goal is to get map data and analyze it for differences
   
   rclcpp::shutdown(); // shutdown ROS
   return 0;
